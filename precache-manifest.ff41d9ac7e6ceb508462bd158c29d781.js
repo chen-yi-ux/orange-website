3e4909136ffa28d40de9cbc473a03769117b9237
Cyer <1678447359@qq.com>
2021-12-17 23:10:47 +0800

    update

---
+++ precache-manifest.ff41d9ac7e6ceb508462bd158c29d781.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d721a615b46a766ce8d8",
+    "url": "/orange-website/css/app.505522ba.css"
+  },
+  {
+    "revision": "0bebefc5a86b6193b565",
+    "url": "/orange-website/css/chunk-vendors.6cca5e52.css"
+  },
+  {
+    "revision": "122472388f457eb778c58cb13a3ab43a",
+    "url": "/orange-website/index.html"
+  },
+  {
+    "revision": "d721a615b46a766ce8d8",
+    "url": "/orange-website/js/app.6bd23036.js"
+  },
+  {
+    "revision": "0bebefc5a86b6193b565",
+    "url": "/orange-website/js/chunk-vendors.29534bdb.js"
+  },
+  {
+    "revision": "039b8204861246b5ef60fba782186777",
+    "url": "/orange-website/manifest.json"
+  },
+  {
+    "revision": "e40f7da90f97ac874bf40079c02a982d",
+    "url": "/orange-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/orange-website/robots.txt"
+  }
+]);
